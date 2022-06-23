e0bd7c6ea88479c5a483c6070bccdf6e0761a6a9
Hariharan B <hariharanb@adobe.com>
2022-06-23 18:03:37 +0530

    Add files via upload

---
+++ TicketDemo/website/models.py
@@ -0,0 +1,44 @@
+from time import timezone
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+from datetime import datetime
+import time
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150),unique=True)
+    username = db.Column(db.String(150),unique=True)
+    password = db.Column(db.String(150))
+    usertype = db.Column(db.String(20))
+    date_created = db.Column(db.DateTime,default = time.strftime("%d %B, %Y %H:%M:%S"))
+    #tickets = db.relationship('Ticket', backref='User',passive_deletes=True)
+    #consulted = db.relationship('Ticket', backref='User',passive_deletes=True)
+    comments = db.relationship('Comment',backref='User',passive_deletes=True)
+
+
+class Ticket(db.Model):
+    __tablename__ = 'Ticket'
+    id = db.Column(db.Integer, primary_key=True)
+    custname = db.Column(db.String(150),unique=True)
+    custreq = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime,default = time.strftime("%d %B, %Y %H:%M:%S"))
+    author_id = db.Column(db.Integer, db.ForeignKey('User.id',ondelete="CASCADE"),nullable=False)
+    consultant_id = db.Column(db.Integer, db.ForeignKey('User.id',ondelete="CASCADE"),nullable=True)
+    author = db.relationship("User",foreign_keys=[author_id])
+    consultant = db.relationship("User",foreign_keys=[consultant_id])
+    status = db.Column(db.String(150))
+    comments = db.relationship('Comment',backref='Ticket',passive_deletes=True)
+    hours = db.Column(db.Float)
+    cost = db.Column(db.Float)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200),nullable=False)
+    date_created = db.Column(db.DateTime,default = time.strftime("%d %B, %Y %H:%M:%S"))
+    author = db.Column(db.Integer, db.ForeignKey(
+        'User.id', ondelete="CASCADE"),nullable=False)
+    ticket_id = db.Column(db.Integer, db.ForeignKey(
+        'Ticket.id', ondelete="CASCADE"),nullable=False)
+    status = db.Column(db.String(20))
---
+++ TicketDemo/website/views.py
@@ -0,0 +1,168 @@
+from unicodedata import category
+from flask import Blueprint, render_template, request,flash,redirect,url_for
+from flask_login import login_required, current_user
+from .models import Ticket, User, Comment
+from . import db
+
+views = Blueprint("views",__name__)
+
+@views.route("/")
+@views.route("/home")
+@login_required
+def home():
+    if current_user.usertype == 'Consultant':
+        tickets = Ticket.query.filter_by(consultant_id=current_user.id).all()
+    else:
+        tickets = Ticket.query.all()
+    return render_template("dashboard.html",user=current_user,tickets=tickets)
+
+
+@views.route("/create-ticket",methods=['GET','POST'])
+@login_required
+def create_ticket():
+    if request.method == "POST":
+        custname = request.form.get('custname')
+        custreq = request.form.get('custreq')
+        status = "Ticket Created"
+        if not custname or not custreq:
+            flash("Please fill all fields",category='error')
+        else:
+            ticket = Ticket(custname=custname,custreq=custreq,author_id = current_user.id,status=status)
+            db.session.add(ticket)
+            db.session.commit()
+            flash("Ticket Created",category='success')
+            return redirect(url_for('views.home'))
+
+
+    return render_template('create_ticket.html',user=current_user)
+
+
+@views.route("/delete-ticket/<id>")
+@login_required
+def delete_ticket(id):
+    ticket = Ticket.query.filter_by(id=id).first()
+
+    if not ticket:
+        flash("Ticket does not exist", category='error')
+    elif current_user.id != ticket.author_id:
+        flash("You do not have permission to delete this ticket",category='error')
+    else:
+        db.session.delete(ticket)
+        db.session.commit()
+        flash('Ticket Deleted',category='success')
+    return redirect(url_for('views.home'))
+
+@views.route("/ticketsby/<username>")
+@login_required
+def ticketsby(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        flash("No user with that username exists",category='error')
+        return redirect(url_for('views.home'))
+
+    tickets = Ticket.query.filter_by(author_id=user.id).all()
+    return render_template("ticketsby.html",user=current_user,tickets=tickets,username=username)
+
+
+@views.route("/tickets/<ticket_id>")
+@login_required
+def tickets(ticket_id):
+    ticket = Ticket.query.filter_by(id=ticket_id).first()
+    if not ticket:
+        flash("Invalid ticket ID",category='error')
+        return redirect(url_for('views.home'))
+    return render_template("current_ticket.html",user=current_user,ticket=ticket)
+
+
+@views.route("/assign-consultant/<id>",methods=['GET','POST'])
+@login_required
+def assign_consultant(id):
+    ticket = Ticket.query.filter_by(id=id).first()
+    consultants = User.query.filter_by(usertype='Consultant').all()
+    if not ticket:
+        flash("Ticket does not exist", category='error')
+    elif current_user.usertype != 'GDC_SPOC':
+        flash("You do not have permission to assign consultant",category='error')
+    else:
+        if request.method == "POST":
+            consultant = request.form.get('consultant-name')
+            status = "Consultant assigned"
+            consultant_id = User.query.filter_by(username=consultant).first().id
+            ticket.consultant_id = consultant_id
+            ticket.status = status
+            db.session.commit()
+            flash("Consultant Assigned",category='success')
+            return redirect(url_for('views.home'))
+    
+    return render_template('assign_consultant.html',user=current_user,consultants = consultants)
+
+
+@views.route("/update-status/<id>",methods=['GET','POST'])
+@login_required
+def update_status(id):
+    ticket = Ticket.query.filter_by(id=id).first()
+    if not ticket:
+        flash("Ticket does not exist", category='error')
+    elif current_user.usertype != 'Consultant':
+        flash("You do not have permission to update status",category='error')
+    elif current_user.id != ticket.consultant_id:
+        flash("You are not the consultant assigned to this ticket",category='error')
+    else:
+        if request.method == "POST":
+            status = request.form.get('status')
+            ticket.status = status
+            db.session.commit()
+            if status == 'Complete':
+                return redirect('/estimated-details/'+str(ticket.id))
+            flash("Status updated",category='success')
+            return redirect(url_for('views.home'))
+    return render_template('update_status.html',user=current_user)
+
+@views.route("create-comment/<ticket_id>",methods=['POST'])
+@login_required
+def create_comment(ticket_id):
+    text = request.form.get('text')
+
+    if not text:
+        flash("Comment cannot be empty",category='error')
+    else:
+        ticket = Ticket.query.filter_by(id=ticket_id)
+        if ticket:
+            comment = Comment(text=text,author=current_user.id,ticket_id=ticket_id)
+            db.session.add(comment)
+            db.session.commit()
+        else:
+            flash("Post does not exist",category='error')
+
+    return redirect(url_for('views.home'))
+
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+       flash("Comment does not exist", category='error')
+    elif current_user.id != comment.author:
+        flash("You are not authorized to delete this comment", category='error')
+    else:
+        comment.status = "Deleted"
+        #db.session.delete(comment)
+        db.session.commit()
+        
+    return redirect(url_for('views.home'))
+
+@views.route("/estimated-details/<ticket_id>",methods=['GET','POST'])
+@login_required
+def estimated_details(ticket_id):
+    ticket = Ticket.query.filter_by(id=ticket_id).first()
+    if request.method == "POST":
+        hours = request.form.get('hours')
+        cost = request.form.get('cost')
+        ticket.hours = hours
+        ticket.cost = cost
+        db.session.commit()
+        flash("Estimated Details Updated",category='success')
+        return redirect(url_for('views.home'))
+    return render_template('estimate_details.html',user=current_user)
