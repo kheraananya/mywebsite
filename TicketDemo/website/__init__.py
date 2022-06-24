4feb710062b6826320bafd5e1b057c37c3df8837
kheraananya <ananyak@adobe.com>
2022-06-24 16:58:41 +0530

    Fixed the Time format

--- TicketDemo/website/__init__.py
+++ TicketDemo/website/__init__.py
@@ -2,15 +2,49 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from datetime import datetime
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "hariharan"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:5416@localhost/TicketDemo'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:252001@localhost/TicketDemo'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+
+    @app.template_filter('humanize')
+    def humanize(time):
+        now = datetime.now()
+        diff = now - time 
+        second_diff = diff.seconds 
+        day_diff = diff.days
+
+        if day_diff < 0:
+            return ''
+
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return str(second_diff) + " seconds ago"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+             return str(second_diff // 60) + " minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return str(second_diff // 3600) + " hours ago"
+        if day_diff == 1:
+            return "Yesterday"
+        if day_diff < 7:
+            return str(day_diff) + " days ago"
+        if day_diff < 31:
+            return str(day_diff // 7) + " weeks ago"
+        if day_diff < 365:
+            return str(day_diff // 30) + " months ago"
+        return str(day_diff // 365) + " years ago"
+
 
     from .views import views
     from .auth import auth
